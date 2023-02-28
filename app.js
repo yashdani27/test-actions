503e95ffea5c7d6d6ea43460374286e3f18a7a79
Yash Dani <yashdani18@gmail.com>
2023-02-27 16:41:51 -0800

    path change

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "/frontend/build")));
 
 app.get("/names", (req, res) => {
   res.send("Yash Dani");
